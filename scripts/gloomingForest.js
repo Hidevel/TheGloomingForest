7c80ee8a5e8e24bed76b73e8113e7841c0f37780
Lucka <tarchooo@gmail.com>
2016-11-22 19:41:50 +0100

    Update gloomingForest.js
    I tried to make it so the trees and mushrooms wouldn't be created new every time the game restarts, but since it still works half as fast every time, I assume I did something wrong. 
    Also, it seems like I messed collision detection somehow. I will try to correct it tomorrow.
    By the way, we will also have to write a report by sunday.

--- scripts/gloomingForest.js
+++ scripts/gloomingForest.js
@@ -7,14 +7,25 @@ Physijs.scripts.worker = 'scripts/physijs_worker.js';
 	var initScene, render, addObjects, 
 	treeCoordGenerator, mushroomPlacer,
 	keysInterpreter, applyMovement, 
-	startNewGame, restart;
+	startNewGame, restart, rearrangeObjects;
 	
 	//event handler functions
 	var keyUp,keyDown,handleCollision;	
 	
 	//scene basics variables
 	var scene, renderer, render_stats, physics_stats, textureLoader, 
-		ambientLight, pointlight, camera, cameraDistanceToPlayer;
+		ambientLight, 
+		pointlightSpiritUp, 
+		pointlightSpiritDown,
+		pointlightSpiritFront,
+		pointlightSpiritBack,
+		pointlightSpiritLeft,
+		pointlightSpiritRight,
+		camera, 
+		cameraDistanceToPlayer3rdPerson,
+		cameraLookAt3rdPerson,
+		cameraDistanceFromPlayerYCoord,
+		cameraLookAt1stPerson;
 	
 		//size variables
 		var mapSize;//size of the map
@@ -28,7 +39,9 @@ Physijs.scripts.worker = 'scripts/physijs_worker.js';
 			randomGeneratorStatic,
 			randomGeneratorDynamic,
 			treesArray, 
-			mushroomsArray;
+			mushroomsArray,
+			chosenTree,
+			alreadyUsedTrees;
 		
 		//geometry, material and mesh variables
 		var ground, groundGeometry, groundMaterial, //ground
@@ -41,6 +54,8 @@ Physijs.scripts.worker = 'scripts/physijs_worker.js';
 	//player related variables
 	var keyboard = {};
 	
+	var brightness, lightIntensity, lightDistance;
+	
 	var cam3; //camera is set in third person view or first person view
 
 	var transparency;
@@ -49,10 +64,8 @@ Physijs.scripts.worker = 'scripts/physijs_worker.js';
 	movement speed, current speed, 
 	rotation speed and current rotation 
 	of the player respectively*/
-	var player = {height: 2.0, 
-					movementSpeed: 30.0, movement:0.0, 
-					rotationSpeed: Math.PI*0.005, rotation:0.0
-	};
+	var player;
+	var noSpinning;
 
 
 /***
@@ -60,7 +73,7 @@ Nice, long descriptions can be written this way
 ***/
 initScene=function() {
 	
-	renderer = new THREE.WebGLRenderer({ antialias: false });
+	renderer = new THREE.WebGLRenderer({ antialias: true });
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	
 	//enable shadows
@@ -82,18 +95,21 @@ initScene=function() {
 		physics_stats.domElement.style.zIndex = 100;
 	document.getElementById( 'viewport' ).appendChild( physics_stats.domElement );
 
-	scene = new Physijs.Scene;
+	scene = new Physijs.Scene({fixedTimeStep:1/60});
 	scene.setGravity(new THREE.Vector3( 0, -100, 0 ));
 	scene.addEventListener(
 		'update',
 		function() {
-		scene.simulate( undefined, 1 );
+		scene.simulate( undefined, 2 );
 		physics_stats.update();
 		}
 	);	
 	
 	camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-	cameraDistanceToPlayer=5;
+	cameraDistanceToPlayer3rdPerson=10;
+	cameraLookAt3rdPerson=4;
+	cameraDistanceFromPlayerYCoord=4;
+	cameraLookAt1stPerson=5;
 	scene.add(camera);
 	
 	
@@ -101,11 +117,13 @@ initScene=function() {
 		
 		mapSize = 100;//size of the map
 		
-		numTrees =200; //number of trees on the map
+		numTrees = 200; //number of trees on the map
 		numMushrooms = 10; //number of mushrooms on the map
 		treesArray=new Array(numTrees);
 		mushroomsArray=new Array(numMushrooms);
 		//player related variables
+		noSpinning = new THREE.Vector3(0,0,0)
+		chosenTree,alreadyUsedTrees=new Array(numMushrooms);
 		
 		cam3 = true; //camera is set in third person view
 		
@@ -116,11 +134,17 @@ initScene=function() {
 				rotationSpeed: Math.PI*0.008, rotation:0.0
 		};
 		
+		//array of used coordinates
+		randomTreeCoordinates = new Array(2);//we will store x and y coordiantes
+			for(var i = 0; i < 2; i++){
+				randomTreeCoordinates[i] = new Array(numTrees); //coordinates for every object
+		}
+				
 		//mushroomPlacer support variables
 		treeTrunkBottom=1;
 		mushroomTreeDistance=1;
 	
-	//treeCoordGenerator support variables
+		//treeCoordGenerator support variables
 		randomGeneratorStatic=(mapSize/2)-treeTrunkBottom-mushroomTreeDistance;
 		randomGeneratorDynamic=randomGeneratorStatic*2;
 	
@@ -173,17 +197,18 @@ initScene=function() {
 						0.8 //restitution
 			);
 			
-			spiritGeometry = new THREE.CylinderGeometry(0.5, 0.5, 2);
+			spiritGeometry = new THREE.CylinderGeometry(0.5, 0.5, player.height,32,32);
 			
 			spiritMaterial = Physijs.createMaterial(
 					new THREE.MeshPhongMaterial(
 					{ color:0x33ff66,
-					opacity: 1,
-					transparent: true
-					
+					specular:0xf4b642,
+					transparent: true,
+					opacity:1,
+					shininess:100					
 					}),
-					0.3, //friction
-					1.0 //restitution
+					0.5, //friction
+					0.0 //restitution
 			);
 	
 	// Ground
@@ -196,40 +221,21 @@ initScene=function() {
 		ground.name='ground';
 		scene.add( ground );
 	
-	ambientLight = new THREE.AmbientLight(0xffffff, 0.4);
+	ambientLight = new THREE.HemisphereLight(0x053327, 0x287260,0.2);
 	scene.add(ambientLight);
-	
-	pointlight = new THREE.PointLight(0xffffff, 1.0);
-	//PointLight( color, intensity, distance, decay )
-	pointlight.position.set(0, 20, 0);
-	pointlight.castShadow = true;
-	//maximum draw distance for the shadows to something reasonable
-	pointlight.shadow.camera.near = 0.1;
-	pointlight.shadow.camera.far = 50;
-	scene.add(pointlight);
-	
+	addObjects();	
 	startNewGame();
 }
 
 //definition: startNewGame
 startNewGame = function(){
-	
-	//array of used coordinates
-	randomTreeCoordinates = new Array(2);//we will store x and y coordiantes
-		for(var i = 0; i < 2; i++){
-			randomTreeCoordinates[i] = new Array(numTrees); //coordinates for every object
-		}
-		
-	
-	
-	addObjects();	
-	
+	rearrangeObjects();
 	scene.simulate();
 	requestAnimationFrame(render);
 	
-}
+};
 	
-	//definition: addObject
+	//definition: addObjects
 	addObjects=function(){
 		
 		//create the player, the spirit
@@ -239,11 +245,9 @@ startNewGame = function(){
 			1
 		);		
 		
-		spirit.setAngularFactor(new THREE.Vector3(0, 0, 0));
-		spirit.position.y += 1.1;
 		spirit.receiveShadow = true; //meshes - object both receive
 		spirit.castShadow = true; // and cast shadows
-		//spirit.setCcdMotionThreshold(1);
+		spirit.setCcdMotionThreshold(1);
 		spirit.setCcdSweptSphereRadius(1);
 		spirit.addEventListener('ready',function(){
 		spirit.addEventListener('collision',handleCollision);
@@ -256,18 +260,39 @@ startNewGame = function(){
 		);
 		scene.addConstraint( constraint );*/
 		
+		//adds lighting to the spirit
+		/*
+		intensity && distance <- at brightness
+		0.2 && 5 <- at min brightness
+		0.4 && 25 <- at max brightness
+		scales:
+			- intensity: 0.2 0.25 0.3 0.35 0.4
+			- distance: 5 10 15 20 25
+		*/
+		pointlightSpiritUp		= 	new THREE.PointLight(0xeeeeff, 0.4, 22,1);
+		pointlightSpiritDown	= 	new THREE.PointLight(0xeeeeff, 0.4, 28,1);
+		pointlightSpiritFront	= 	new THREE.PointLight(0xeeeeff, 0.4, 25,1);
+		pointlightSpiritBack	= 	new THREE.PointLight(0xeeeeff, 0.4, 25,1);
+		pointlightSpiritLeft	= 	new THREE.PointLight(0xeeeeff, 0.4, 25,1);
+		pointlightSpiritRight	= 	new THREE.PointLight(0xeeeeff, 0.4, 25,1); //PointLight( color, intensity, distance, decay )
 		
-		scene.add(spirit);			
+		pointlightSpiritUp.position.set(0, player.height+2, 0);
+		pointlightSpiritDown.position.set(0, player.height/20, 0);
+		pointlightSpiritFront.position.set(0, player.height/2, 3);
+		pointlightSpiritBack.position.set(0, player.height/2, -3);
+		pointlightSpiritLeft.position.set(-3, player.height/2, 0);
+		pointlightSpiritRight.position.set(3, player.height/2, 0);
+	
+		spirit.add(pointlightSpiritUp);
+		spirit.add(pointlightSpiritDown);
+		spirit.add(pointlightSpiritFront);
+		spirit.add(pointlightSpiritBack);
+		spirit.add(pointlightSpiritLeft);
+		spirit.add(pointlightSpiritRight);
+					
 		
 		//place trees and mushrooms on the map
 		for (var i = 0; i < numTrees; i++) {
-			
-			randomTreeCoordinates[0][i]=treeCoordGenerator(randomTreeCoordinates[0]);		
-			randomTreeCoordinates[1][i]=treeCoordGenerator(randomTreeCoordinates[1]);
-			
-			//NO TREE APPEARING ON 0, 0, 0 where the spirit is
-			if (Math.abs(randomTreeCoordinates[0][i]) < 2.0) randomTreeCoordinates[0][i] += 4.0;
-			if (Math.abs(randomTreeCoordinates[1][i]) < 2.0) randomTreeCoordinates[1][i] += 4.0;
 			
 			tree = new Physijs.CylinderMesh(
 				trunkGeometry,
@@ -275,11 +300,11 @@ startNewGame = function(){
 				0
 			);
 			
-			tree.position.set (randomTreeCoordinates[0][i],
+			tree.position.set (i/10 - mapSize/2,
 								3,
-								randomTreeCoordinates[1][i]
+								i/10 - mapSize/2
 			);
-				
+			
 			branches = new Physijs.SphereMesh(
 				branchGeometry, 
 				branchMaterial,
@@ -295,13 +320,70 @@ startNewGame = function(){
 				
 			tree.name='tree'+i;
 			treesArray[i]=tree;
-			scene.add(tree);
 		}
-
+		
+		//create mushrooms
+		for(var i=0;i<numMushrooms;i++){
+			//create mushroom trunk
+			mushroom = new Physijs.CylinderMesh(
+				mushroomTrunkGeometry,
+				mushroomTrunkMaterial,
+				0
+			);
+			
+			mushroom.position.set (i/6 - mapSize/2,
+								0.125,
+								i/6 - mapSize/2
+			);
+			
+		//create mushroom cap
+		mushroomCap = new Physijs.ConeMesh(
+			mushroomCapGeometry, 
+			mushroomcapMaterial,
+			1
+		);
+			//cap above trunk
+		mushroomCap.position.set (0,0.25,0);
+			
+			//combine cap, with trunk
+		mushroom.add(mushroomCap);
+			
+		mushroom.receiveShadow = true;
+		mushroom.castShadow = true;
+			
+		mushroom.name='mushroom'+i;
+		mushroom.touchEffect=""; //what happens when the player touches the mushroom
+		
+		mushroomsArray[i]=mushroom;//store the mushrooms in the mushroom array
+		}
+	};
+	
+	//definition: rearrangeObjects
+	rearrangeObjects = function() {
+		spirit.setAngularFactor(noSpinning);
+		spirit.position.set(0, player.height/2 + 0.1, 0);
+		scene.add(spirit);
+		
+		//place trees and mushrooms on the map
+		for (var i = 0; i < numTrees; i++) {
+			
+			randomTreeCoordinates[0][i]=treeCoordGenerator(randomTreeCoordinates[0]);		
+			randomTreeCoordinates[1][i]=treeCoordGenerator(randomTreeCoordinates[1]);
+			
+			//NO TREE APPEARING ON 0, 0, 0 where the spirit is
+			if (Math.abs(randomTreeCoordinates[0][i]) < 2.0) randomTreeCoordinates[0][i] += 4.0;
+			if (Math.abs(randomTreeCoordinates[1][i]) < 2.0) randomTreeCoordinates[1][i] += 4.0;
+			
+			treesArray[i].position.set (randomTreeCoordinates[0][i],
+								3,
+								randomTreeCoordinates[1][i]
+			);
+			scene.add(treesArray[i]);
+			
+		}
 		//place a mushroom near trees
 		mushroomPlacer(treesArray,numTrees,numMushrooms);
-			
-	}
+	};
 		
 		//generataes a random coordinate
 		treeCoordGenerator = function (storedCoordiantes){
@@ -332,10 +414,8 @@ startNewGame = function(){
 		//places numOfMushrooms mushrooms near randomly chosen trees
 		mushroomPlacer=function (treeArray,treeArrayLength,numOfMushrooms){
 			
-			var chosenTree,alreadyUsedTrees=new Array(numOfMushrooms);
-			
 			for(var i=0;i<numOfMushrooms;i++){
-				
+				scene.remove(mushroomsArray[i]);
 				//select a tree from the treeArray
 				do{
 					chosenTree=treeArray[Math.floor(Math.random() * treeArrayLength)];
@@ -343,68 +423,39 @@ startNewGame = function(){
 				}while(alreadyUsedTrees.includes(chosenTree));
 				
 				alreadyUsedTrees[i]=(chosenTree); //add the chosen tree to the used ones
-				
-				
-					//create mushroom trunk
-				mushroom = new Physijs.CylinderMesh(
-					mushroomTrunkGeometry,
-					mushroomTrunkMaterial,
-					0
-				);
 					
 					//variety of mushrooms placements through the number of the current mushroom
 					//place the mushrooms near the trees
 				var mushroomDirection=i % 4;
 				if(mushroomDirection === 0){//place the mushroom north-east from tree
-					mushroom.position.set (
+					mushroomsArray[i].position.set (
 						chosenTree.position.x+treeTrunkBottom+mushroomTreeDistance,
 						0.25,
 						chosenTree.position.z+treeTrunkBottom+mushroomTreeDistance
 					);
 				}else if(mushroomDirection === 1){//place the mushroom north-west from tree
-					mushroom.position.set (
+					mushroomsArray[i].position.set (
 						chosenTree.position.x-treeTrunkBottom-mushroomTreeDistance,
 						0.25,
 						chosenTree.position.z+treeTrunkBottom+mushroomTreeDistance
 					);
 				}else if(mushroomDirection === 2){//place the mushroom south-east from tree
-					mushroom.position.set (
+					mushroomsArray[i].position.set (
 						chosenTree.position.x+treeTrunkBottom+mushroomTreeDistance,
 						0.25,
 						chosenTree.position.z-treeTrunkBottom-mushroomTreeDistance
 					);
 				}else if(mushroomDirection === 3){//place the mushroom south-west from tree
-					mushroom.position.set (
+					mushroomsArray[i].position.set (
 						chosenTree.position.x-treeTrunkBottom-mushroomTreeDistance,
 						0.25,
 						chosenTree.position.z-treeTrunkBottom-mushroomTreeDistance
 					);
 				}
-					
-					//create mushroom cap
-				mushroomCap = new Physijs.ConeMesh(
-					mushroomCapGeometry, 
-					mushroomcapMaterial,
-					1
-				);
-					//cap above trunk
-				mushroomCap.position.set (0,0.25,0);
-					
-					//combine cap, with trunk
-				mushroom.add(mushroomCap);
-					
-				mushroom.receiveShadow = true;
-				mushroom.castShadow = true;
-					
-				mushroom.name='mushroom'+i;
-				mushroom.touchEffect=""; //what happens when the player touches the mushroom
-				
-				mushroomsArray[i]=mushroom;//store the mushrooms in the mushroom array
-				
 				scene.add(mushroom);
+				
+			}
 		}
-		
-}
 
 //RENDERER
 
@@ -413,7 +464,7 @@ render = function() {
 				
 		keysInterpreter();//interprets user input
 		applyMovement();
-				
+			
 		renderer.render( scene, camera );
 		render_stats.update();
 };
@@ -432,14 +483,15 @@ render = function() {
 			//movement
 		if (keyboard[38]){ // up arrow key	
 			player.movement=player.movementSpeed;
-			if (Math.abs(spirit.position.x) >= Math.abs(mapSize/2 + 0.5) || Math.abs(spirit.position.z) >= Math.abs(mapSize/2 + 0.5) ){		
+			if (Math.abs(spirit.position.x) >= Math.abs(mapSize/2 + 0.5) 
+				|| Math.abs(spirit.position.z) >= Math.abs(mapSize/2 + 0.5) ){		
 				restart();
 			}
-			
 		}
 		if (keyboard[40]){ // down arrow key
 			player.movement=-player.movementSpeed;
-			if (Math.abs(spirit.position.x) >= Math.abs(mapSize/2 + 0.5) || Math.abs(spirit.position.z) >= Math.abs(mapSize/2 + 0.5) ){
+			if (Math.abs(spirit.position.x) >= Math.abs(mapSize/2 + 0.5) 
+				|| Math.abs(spirit.position.z) >= Math.abs(mapSize/2 + 0.5) ){
 				restart();
 			}
 		}
@@ -448,22 +500,16 @@ render = function() {
 			player.movement=0.0;
 		}
 		transparency -= 0.001;
-		console.log(transparency);
 		if (transparency <= 0.0) restart();
 		else spirit.material.opacity = transparency;
-		
 	}
 	
 	restart = function(){
 		//fade out
 		transparency = 1;
 		spiritMaterial.opacity = transparency;
-		
-		for(var i=0;i<numMushrooms;i++){
-			scene.remove(mushroomsArray[i]);
-		}
-		for(var i=0;i<numTrees;i++){
-			scene.remove(treesArray[i]);
+		for (var j = 0; j < numTrees; j++) {
+			scene.remove(treesArray[j]);
 		}
 		scene.remove(spirit);
 		startNewGame();
@@ -480,30 +526,38 @@ render = function() {
 				)
 			);
 		
-		spirit.setAngularFactor(new THREE.Vector3(0,0,0)); //prevent the player character from spinning
+		spirit.setAngularFactor(noSpinning); //prevent the player character from spinning
 		
 			//follow the players movement form 3rd or first person
 		if(cam3){
 			camera.position.set(
-				spirit.position.x - (Math.sin(player.rotation)*cameraDistanceToPlayer),
-				player.height + 1,
-				spirit.position.z - (Math.cos(player.rotation)*cameraDistanceToPlayer)
+				spirit.position.x - (Math.sin(player.rotation)*cameraDistanceToPlayer3rdPerson),
+				player.height + cameraDistanceFromPlayerYCoord,
+				spirit.position.z - (Math.cos(player.rotation)*cameraDistanceToPlayer3rdPerson)
+			);
+			
+			//look where the player looks in 3rd person
+			camera.lookAt(
+			new THREE.Vector3(
+				spirit.position.x + Math.sin(player.rotation)*cameraLookAt3rdPerson,
+				player.height,
+				spirit.position.z + Math.cos(player.rotation)*cameraLookAt3rdPerson
+				)
 			);
 		}
 		else{
 			camera.position.set(spirit.position.x,
-								player.height,
+								player.height-(player.height/20),
 								spirit.position.z);
-		}
-			
 			//look where the player looks
-		camera.lookAt(
-		new THREE.Vector3(
-			spirit.position.x + Math.sin(player.rotation)*cameraDistanceToPlayer,
-			player.height,
-			spirit.position.z + Math.cos(player.rotation)*cameraDistanceToPlayer
-			)
-		);
+			camera.lookAt(
+			new THREE.Vector3(
+				spirit.position.x + Math.sin(player.rotation)*cameraLookAt1stPerson,
+				1,
+				spirit.position.z + Math.cos(player.rotation)*cameraLookAt1stPerson
+				)
+			);
+		}
 	}
 		
 //EVENT HANDLER FUNCTIONS
@@ -526,14 +580,11 @@ keyUp=function(event){
 }
 
 handleCollision=function(collided_with){
-	
+	console.log(collided_with.name);
 	if(collided_with.name.includes("mushroom")){
-		for(var i=0;i<numMushrooms;i++){
-			transparency += 0.02;
-			if (transparency > 1.0) transparency = 1;
-			spirit.material.opacity = transparency;
-			scene.remove(mushroomsArray[i]);
-		}
+		transparency += 0.02;
+		if (transparency > 1.0) transparency = 1.0;
+		spirit.material.opacity = transparency;
 		mushroomPlacer(treesArray,numTrees,numMushrooms);
 	}
 };
